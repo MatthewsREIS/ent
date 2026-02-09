7eb314b39f0532e70179ac6b66f0ca25bfb73516
codelite7 <zack.st7@gmail.com>
2026-02-09 09:25:30 -0700

    entc/integration/privacy: move model convenience calls to clients

--- entc/integration/privacy/privacy_test.go
+++ entc/integration/privacy/privacy_test.go
@@ -48,35 +48,35 @@ func TestPrivacyRules(t *testing.T) {
 		client.Team.Create().SetName("ent-contrib"),
 	).SaveX(admin)
 
-	_, err = client.User.Create().SetName("a8m").AddTeams(teams[0]).Save(view)
+	_, err = client.User.Create().SetName("a8m").AddTeamIDs(teams[0].ID).Save(view)
 	require.True(t, errors.Is(err, privacy.Deny), "user creation requires admin user")
-	a8m := client.User.Create().SetName("a8m").AddTeams(teams[0], teams[1]).SaveX(admin)
-	nat := client.User.Create().SetName("nati").AddTeams(teams[1]).SaveX(admin)
+	a8m := client.User.Create().SetName("a8m").AddTeamIDs(teams[0].ID, teams[1].ID).SaveX(admin)
+	nat := client.User.Create().SetName("nati").AddTeamIDs(teams[1].ID).SaveX(admin)
 
-	_, err = client.Task.Create().SetTitle("task 1").AddTeams(teams[0]).SetOwner(a8m).Save(ctx)
+	_, err = client.Task.Create().SetTitle("task 1").AddTeamIDs(teams[0].ID).SetOwnerID(a8m.ID).Save(ctx)
 	require.True(t, errors.Is(err, privacy.Deny), "task creation requires viewer/owner match")
 
 	a8mctx := viewer.NewContext(view, &viewer.UserViewer{User: a8m, Role: viewer.View | viewer.Edit})
-	client.Task.Create().SetTitle("task 1").AddTeams(teams[0]).SetOwner(a8m).SaveX(a8mctx)
-	_, err = client.Task.Create().SetTitle("task 2").AddTeams(teams[1]).SetOwner(nat).Save(a8mctx)
+	client.Task.Create().SetTitle("task 1").AddTeamIDs(teams[0].ID).SetOwnerID(a8m.ID).SaveX(a8mctx)
+	_, err = client.Task.Create().SetTitle("task 2").AddTeamIDs(teams[1].ID).SetOwnerID(nat.ID).Save(a8mctx)
 	require.True(t, errors.Is(err, privacy.Deny), "task creation requires viewer/owner match")
 
 	natctx := viewer.NewContext(view, &viewer.UserViewer{User: nat, Role: viewer.View | viewer.Edit})
-	client.Task.Create().SetTitle("task 2").AddTeams(teams[1]).SetOwner(nat).SaveX(natctx)
+	client.Task.Create().SetTitle("task 2").AddTeamIDs(teams[1].ID).SetOwnerID(nat.ID).SaveX(natctx)
 
 	tasks := client.Task.Query().AllX(a8mctx)
 	require.Len(t, tasks, 2, "returned tasks from teams 1, 2")
 	task2 := client.Task.Query().OnlyX(natctx)
 	require.Equal(t, "task 2", task2.Title, "returned tasks must be from the same team")
 
-	task3 := client.Task.Create().SetTitle("multi-team-task (1, 2)").AddTeams(teams...).SetOwner(a8m).SaveX(a8mctx)
-	_, err = task3.Update().SetStatus(task.StatusClosed).Save(natctx)
+	task3 := client.Task.Create().SetTitle("multi-team-task (1, 2)").AddTeamIDs(teams[0].ID, teams[1].ID).SetOwnerID(a8m.ID).SaveX(a8mctx)
+	_, err = client.Task.UpdateOne(task3).SetStatus(task.StatusClosed).Save(natctx)
 	require.True(t, errors.Is(err, privacy.Deny), "viewer 2 is not allowed to change the task status")
 
 	// DecisionContext returns a new context from the parent with a decision attached to it.
-	task3.Update().SetStatus(task.StatusClosed).SaveX(privacy.DecisionContext(natctx, privacy.Allow))
-	task3.Update().SetStatus(task.StatusClosed).SaveX(a8mctx)
+	client.Task.UpdateOne(task3).SetStatus(task.StatusClosed).SaveX(privacy.DecisionContext(natctx, privacy.Allow))
+	client.Task.UpdateOne(task3).SetStatus(task.StatusClosed).SaveX(a8mctx)
 	// Update description is allowed for other users in the team.
-	task3.Update().SetDescription("boring description").SaveX(natctx)
-	task3.Update().SetDescription("boring description").SaveX(a8mctx)
+	client.Task.UpdateOne(task3).SetDescription("boring description").SaveX(natctx)
+	client.Task.UpdateOne(task3).SetDescription("boring description").SaveX(a8mctx)
 }
